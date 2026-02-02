315bb5a71e250a34c21c7c472b9afe7efa8930b8
gege-xia <GEGEXIA94@GMAIL.COM>
2026-02-02 19:26:53 +0100

    add contact email template

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,15 +1,5 @@
-<x-layout><x-flash />
-    <form method="POST" action="{{ route('contact.store') }}">
-
-        @csrf
-        <input class="form-control mb-2" name="name" placeholder="Nome">
-        <input class="form-control mb-2" name="email" placeholder="Email">
-        <textarea class="form-control mb-2" name="message" placeholder="Messaggio"></textarea>
-        <h2>Nuovo messaggio</h2>
-        <p>Nome: {{ $data['name'] }}</p>
-        <p>Email: {{ $data['email'] }}</p>
-        <p>Messaggio: {{ $data['message'] }}</p>
-        <button class="btn btn-primary">Invia</button>
-
-    </form>
-</x-layout>
+<h2>Nuovo messaggio Manicolandia</h2>
+<p><b>Nome:</b> {{ $data['name'] }}</p>
+<p><b>Email:</b> {{ $data['email'] }}</p>
+<p><b>Messaggio:</b> {{ $data['message'] }}</p>
+<p>— inviato dal sito 🎭</p>
