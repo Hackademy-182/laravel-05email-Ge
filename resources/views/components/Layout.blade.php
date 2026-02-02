5fc52d1342d2d2781899e9a5277da1b64c54ae6d
gege-xia <GEGEXIA94@GMAIL.COM>
2026-02-02 17:57:22 +0100

    add blade components layout topbar flash

--- resources/views/components/Layout.blade.php
+++ resources/views/components/Layout.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="it">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Email</title>
-    @vite('resources/css/app.css', 'resources/js/app.js')
+    <meta charset="utf-8">
+    <title>{{ $title ?? 'Manicolandia' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    {{ $slot }}
-</body>
+<body class="p-4">{{ $slot }}</body>
 
 </html>
---
+++ resources/views/components/topbar.blade.php
@@ -0,0 +1,4 @@
+<nav class="d-flex gap-3 mb-4">
+    <a href="{{ route('home') }}">Home</a>
+    <a href="{{ route('contact.create') }}">Contatti</a>
+</nav>
