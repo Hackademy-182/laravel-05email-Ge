11f668304defa6f727f3481377f886fc8de5e036
gege-xia <GEGEXIA94@GMAIL.COM>
2026-02-02 17:51:48 +0100

    add named routes + controllers

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [PageController::class, 'home'])->name('home');
+route::get('/contatti', [ContactController::class, 'create'])->name('contact.create');
+Route::post('/contatti', [ContactController::class, 'store'])->name('contact.store');
