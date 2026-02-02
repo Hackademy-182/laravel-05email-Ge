24a6ac45163af2cad29a3345419ba65758b8c009
gege-xia <GEGEXIA94@GMAIL.COM>
2026-02-02 18:59:28 +0100

    fix contact controller + contact view

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('contact');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate(['name' => 'required', 'email' => 'required|email', 'message' => 'required']);
+        Mail::to(config('mail.from.address'))->send(new ContactMail($data));
+
+        return back()->with('success', 'Inviato! 🎭');
+    }
 }
