1afcb10efee46d99f1590724304376a9e01406cb
gege-xia <GEGEXIA94@GMAIL.COM>
2026-02-02 18:25:55 +0100

    fix controller imports in routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/', [PageController::class, 'home'])->name('home');
-route::get('/contatti', [ContactController::class, 'create'])->name('contact.create');
+Route::get('/contatti', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/contatti', [ContactController::class, 'store'])->name('contact.store');
